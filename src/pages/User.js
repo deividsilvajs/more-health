af28aa7efd351300c13932165e340a5804f2d2f2
Deivid Silva <imdeividsilva@gmail.com>
2023-06-16 13:14:33 -0300

    troca do diretório do User

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Content from './components/user/Content';
-import User from './components/user/User';
+import User from '../User/User';
 
 function Page() {
 
