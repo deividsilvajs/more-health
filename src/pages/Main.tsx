6fb529efc19a2332fda58d315a2970541c7c1dfb
Deivid Silva <imdeividsilva@gmail.com>
2023-07-05 20:17:52 -0300

    Main troca por typescript

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import About from '../components/main/About'
 import Buttons from '../components/main/Buttons'
 import LoginForm from '../components/main/forms/LoginForm'
