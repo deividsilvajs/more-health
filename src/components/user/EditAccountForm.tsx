bd5e4a8a845653076a07f21acbe35d1fca8d3be9
Deivid Silva <imdeividsilva@gmail.com>
2023-07-09 15:46:41 -0300

    elementos que poderão ser alterados

--- src/components/user/EditAccountForm.tsx
+++ src/components/user/EditAccountForm.tsx
@@ -6,8 +6,22 @@ const EditAccountForm = ({ hide }: Props) => {
 
     return (
         <div onClick={e => hideForm(e, hide)} className='form'>
-            <div className='card p-5'>
-                Hello, World
+            <div className='card py-3 d-flex flex-column align-items-center'>
+                <div className='input-group w-50'>
+                    <label className='input-group-text'>Nome</label>
+                    <input className='form-control' type='text' />
+                </div>
+                <div className='d-flex justify-content-around my-4'>
+                    <div className='d-flex flex-column align-items-center'>
+                        <label>Peso</label>
+                        <input className='w-25' type='text' required />
+                    </div>
+                    <div className='d-flex flex-column align-items-center'>
+                        <label>Altura (cm)</label>
+                        <input className='w-25' type='text' required />
+                    </div>
+                </div>
+                <button className='btn btn-primary'>Confirmar</button>
             </div>
         </div>
     )
